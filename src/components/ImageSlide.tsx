9f9547120ce4ea1dd8e5e7b1d49c79b084375247
Brady Baker <bbaker@homewoodresort.com>
2026-05-02 16:38:13 -0700

    Website scaling to screen better

--- src/components/ImageSlide.tsx
+++ src/components/ImageSlide.tsx
@@ -1,16 +1,12 @@
 function ImageSlide({imgURLS}: {imgURLS: string[]}) {
-    const imgWidth = 400;
-    const margin = 10;
-    const width = imgURLS.length * (imgWidth + margin);
 
     const loopURLS = [...imgURLS, ...imgURLS]
     return(
         <div className="image-slide">
-            <div className="track" style={{"--scroll-width": `-${width}px`} as React.CSSProperties}>
+            <div className="track">
                  {loopURLS.map((curURL, index) => {
-                    console.log(curURL)
                     return (
-                        <img className='slide-img' src={curURL} key={index} style={{width: imgWidth, marginRight: margin}}/>
+                        <img className='slide-img' src={curURL} key={index}/>
                     )
                 })}
             </div>
