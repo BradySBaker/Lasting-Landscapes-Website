a32014a755512e1667f411d48ee25bd0cbbec3ac
Brady B <bradysamuelbaker@gmail.com>
2026-05-07 22:22:20 -0700

    Gallery setup, image selection started

--- src/pages/Ironworks.tsx
+++ src/pages/Ironworks.tsx
@@ -2,18 +2,34 @@ import {useEffect, useState} from 'react'
 
 function Ironworks() {
 
-    const [setIronWorkURLS, ironWorksURLS] = useState([]);
+    const [ironWorkURLS, setIronWorkURLS] = useState<string[]>([]);
+    const [selectedImgURL, setSelectedImgURL] = useState('');
+
+    const imageClick = (e: React.MouseEvent<HTMLImageElement>) => {
+        const src = e.currentTarget.src;
+        setSelectedImgURL(src);
+    }
 
     useEffect(() => {
-        const curURLS = [];
+        const curURLS: string[] = [];
         for (let i = 1; i <= 28; i++ ) {
-            curURLS.push('https://res.cloudinary.com/dztqjtask/image/upload/f_auto,q_auto/Ironworks/i');
+            // curURLS.push(`https://res.cloudinary.com/dztqjtask/image/upload/f_auto,q_auto/Ironworks/${i}`); //Production
+            curURLS.push(`../public/IronWorks/${i}.jpg`); //DEV
         }
+        setIronWorkURLS(curURLS);
     }, [])
 
     return (
         <div className="ironworks">
+            {selectedImgURL && <img src={selectedImgURL}/>}
             <h2>Custom Fabrication</h2>
+            <div className='ironworks-img-container'>
+            {ironWorkURLS.map((curURL, index) => {
+                return (
+                    <img className='ironworks-img' src={curURL} onClick={imageClick}/>
+                )
+            })}
+            </div>
         </div>
     )
 }
