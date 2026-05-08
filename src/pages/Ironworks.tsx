51c266a7e2272ea9b57613310bf7d4df6318e817
Brady B <bradysamuelbaker@gmail.com>
2026-05-07 21:45:45 -0700

    Started work

--- src/pages/Ironworks.tsx
+++ src/pages/Ironworks.tsx
@@ -1,4 +1,15 @@
+import {useEffect, useState} from 'react'
+
 function Ironworks() {
+
+    const [setIronWorkURLS, ironWorksURLS] = useState([]);
+
+    useEffect(() => {
+        const curURLS = [];
+        for (let i = 1; i <= 28; i++ ) {
+            curURLS.push('https://res.cloudinary.com/dztqjtask/image/upload/f_auto,q_auto/Ironworks/i');
+        }
+    }, [])
 
     return (
         <div className="ironworks">
