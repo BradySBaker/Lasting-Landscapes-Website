8bf6dcab1c13f88f3e30523ad111e13e4dcc3d53
Brady B <bradysamuelbaker@gmail.com>
2026-05-07 20:43:31 -0700

    Landscaping page started

--- src/pages/Landscaping.tsx
+++ src/pages/Landscaping.tsx
@@ -1,5 +1,5 @@
 import ImageSlide from "../components/ImageSlide";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type LandscapingURLs = {
   landscapingPatio: string[];
@@ -10,15 +10,19 @@ type LandscapingURLs = {
 };
 
 function Landscaping() {
-
-
+    const landscapingCategories = ['Patios & Outdoor Living', 'Planting & Softscapes', 'Stonework & Grading', 'Walkways & Pathways', 'Water Features & Ponds'];
 
     return (
         <div className="landscaping">
-            <h2>Landscaping</h2>
-
-              <h2 className="image-slide-title">Lasting Landscapes</h2>
-              <ImageSlide imgURLS={['none']}/>
+            <h2>Landscaping Gallery</h2>
+            {landscapingCategories.map((curCat) => {
+                return(
+                <div className="image-slide-container">
+                    <h2 className="image-slide-title">{curCat}</h2>
+                    <ImageSlide imgURLS={['URLS']}/>
+                </div>
+                )
+            })}
         </div>
     )
 }
