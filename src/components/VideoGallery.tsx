bac3170cb8f307f83adcf3122f5a565da5da33a3
Brady Baker <bbaker@homewoodresort.com>
2026-09-07 15:21:40 -0700

    Yotube drone video displaying inside video gallery

--- src/components/VideoGallery.tsx
+++ src/components/VideoGallery.tsx
@@ -10,8 +10,15 @@ function VideoGallery() {
                     <img className='selected-image' src={selectedVidURL}/>
                 </div>
             }
-            
             <div className='gallery-img-container'>
+                <div className="video-container">
+                    <iframe
+                        src="https://www.youtube.com/embed/__RqreA33rg?si=xuuS8dW9Heu1m3vD"
+                        title="Landscape Project"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    />
+                </div>
             </div>
         </div>
     )
