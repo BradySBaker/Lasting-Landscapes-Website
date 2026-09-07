20c884d7fbf339e63ec81d54230caacd94264626
Brady Baker <bbaker@homewoodresort.com>
2026-09-07 15:28:15 -0700

    Added lil modularity for video gallery

--- src/components/VideoGallery.tsx
+++ src/components/VideoGallery.tsx
@@ -1,24 +1,21 @@
-import {useState} from 'react'
-
 function VideoGallery() {
-    const [selectedVidURL, setSelectedVidURL] = useState('');
+    const videoUrls: string[] = ['https://www.youtube.com/embed/__RqreA33rg?si=xuuS8dW9Heu1m3vD'];
 
     return (
         <div className="gallery">
-            {selectedVidURL && 
-                <div className='backdrop' onClick={() => setSelectedVidURL('')}>
-                    <img className='selected-image' src={selectedVidURL}/>
-                </div>
-            }
             <div className='gallery-img-container'>
-                <div className="video-container">
-                    <iframe
-                        src="https://www.youtube.com/embed/__RqreA33rg?si=xuuS8dW9Heu1m3vD"
-                        title="Landscape Project"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    />
-                </div>
+                {videoUrls.map((url, key) => {
+                    return (
+                        <div className="video-container" key={key}>
+                            <iframe
+                                src={url}
+                                title="Landscape Project"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            />
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
